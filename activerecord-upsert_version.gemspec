0d6bc9ee94281c459e0f89e2048807c3a1524285
Sam Stickland <samstickland@spacething.org>
2019-11-29 12:35:44 +0000

    Change tests to use postgres rather than sqlite
    This uses the pg_tester gem which spins up a temporary postgres database
    in /tmp. This requires that the host has postgres installed.

--- activerecord-upsert_version.gemspec
+++ activerecord-upsert_version.gemspec
@@ -32,9 +32,10 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "activerecord"
   spec.add_development_dependency "bundler"
+  spec.add_development_dependency "byebug"
   spec.add_development_dependency "codecov"
   spec.add_development_dependency "ffaker"
+  spec.add_development_dependency "pg_tester"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
-  spec.add_development_dependency "sqlite3"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "bundler/setup"
 require "upsert_version"
 require 'support/models'
+require 'pg_tester'
+require 'byebug'
 
 if ENV['CI'] == 'true'
   require 'simplecov'
@@ -10,11 +12,26 @@ if ENV['CI'] == 'true'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
-ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
-
-load File.dirname(__FILE__) + '/schema.rb'
-
 RSpec.configure do |config|
+  config.before(:suite) do
+    psql = PgTester.new(database: 'upsert_version')
+    psql.setup
+
+    ActiveRecord::Base.establish_connection(
+      adapter: "postgresql",
+      host: psql.host,
+      database: psql.database,
+      user: psql.user,
+      port: psql.port
+    )
+
+    load File.dirname(__FILE__) + '/schema.rb'
+  end
+
+  config.after(:suite) do
+    PgTester.new(database: 'upsert_version').teardown
+  end
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
