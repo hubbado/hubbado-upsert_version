a3e79f3c3e7e6b9711b41e548264da4f5749d59c
Alfonso Uceda <alfonso@hubbado.com>
2023-01-28 17:27:31 +0100

    Fix YAM.load in specs

--- spec/support/db/establish_connection.rb
+++ spec/support/db/establish_connection.rb
@@ -1,3 +1,3 @@
 ENV['RACK_ENV'] = 'test'
-db_yml = YAML.load(ERB.new(File.read('db/config.yml')).result)
+db_yml = YAML.load(ERB.new(File.read('db/config.yml')).result, aliases: true)
 ActiveRecord::Base.establish_connection db_yml["test"]
